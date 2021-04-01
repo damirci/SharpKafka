9018b06975b854efeadb3f81e8fb8f3b4a9dc7b0
Damirchi <m.nami@hotmail.com>
2021-04-01 13:20:39 +0300

    web sample initial

---
+++ samples/Web/Controllers/DateTimeController.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SharpKafka.Producer;
+using System;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DateTimeController : ControllerBase
+    {
+        private readonly ILogger<DateTimeController> _logger;
+        private readonly IKafkaDependentProducer<Null, string> _kafkaDependentProducer;
+
+        public DateTimeController(ILogger<DateTimeController> logger, IKafkaDependentProducer<Null,string> kafkaDependentProducer)
+        {
+            _logger = logger;
+            _kafkaDependentProducer = kafkaDependentProducer;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var message = $"Date: {DateTime.Now}";
+            var dateTopic = "topic-name";
+
+            _logger.LogInformation($"{dateTopic}: {message}");
+
+            await _kafkaDependentProducer.ProduceAsync(dateTopic, new Message<Null, string> { Value = message });
+            return Ok();
+        }
+    }
+}
